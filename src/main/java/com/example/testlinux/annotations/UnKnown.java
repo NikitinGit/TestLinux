5d153effe012347dfa96d13c431ae7f587f0f95f
igor <andnikitn5@gmail.com>
2025-05-16 12:32:56 +0500

    создал объект через рефлексию

--- src/main/java/com/example/testlinux/annotations/UnKnown.java
+++ src/main/java/com/example/testlinux/annotations/UnKnown.java
@@ -2,14 +2,20 @@ package com.example.testlinux.annotations;
 
 import lombok.Getter;
 
-@Getter
+
 public class UnKnown {
+    private UnKnown(){}
+
     @MyAnnotation(name = "UnKnown_name", length = 625)
-    private String name;
+    private String name = " ";
 
     @MyAnnotation(name = "UnKnown_age", length = 785)
     private int age;
 
     @MyAnnotation
     private String address;
+
+    public String getName(){
+        return name + age;
+    }
 }
