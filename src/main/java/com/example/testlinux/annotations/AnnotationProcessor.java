2351c5b419e011415d7970126082c61a3bfdf9b4
igor <andnikitn5@gmail.com>
2025-05-27 19:05:20 +0500

    third-branch commit 1

--- src/main/java/com/example/testlinux/annotations/AnnotationProcessor.java
+++ src/main/java/com/example/testlinux/annotations/AnnotationProcessor.java
@@ -6,7 +6,7 @@ import java.lang.reflect.InvocationTargetException;
 
 public class AnnotationProcessor {
     public static void main(String[] args) {
-        System.out.println("second branch commit 1");
+        System.out.println("master commit 1");
         Person pers = new Person(); System.out.println("it not work pers.getAge(); " + pers.getAge());
         // Получаем все поля класса Person
         test(UnKnown.class);
