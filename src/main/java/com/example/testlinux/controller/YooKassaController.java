80da185af899a605e700f8cd5fd8fe9026dd0e75
igor <andnikitn5@gmail.com>
2025-05-28 14:17:24 +0500

    delete YooKassaService

--- src/main/java/com/example/testlinux/controller/YooKassaController.java
+++ src/main/java/com/example/testlinux/controller/YooKassaController.java
@@ -2,14 +2,11 @@ package com.example.testlinux.controller;
 
 import com.example.testlinux.dto.SimpleResponseDto;
 import com.example.testlinux.dto.YookassaDto;
-import com.example.testlinux.service.YooKassaService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -25,18 +22,9 @@ import java.util.List;
         "2a02:5180::"
 })*/
 public class YooKassaController {
-    private final YooKassaService yooKassaService;
 
     @PostMapping("/youkassa")
     public ResponseEntity<SimpleResponseDto> handleNotification(@RequestBody YookassaDto yoo, HttpServletRequest request) {
-        log.info("handleNotification() yoo; {}", yoo.getId());
-        log.info("request.getRemoteAddr(); {}", request.getRemoteAddr());
-
-        if (!yooKassaService.isIpAllowed(request.getRemoteAddr())) {
-            log.info("ERROR YOOKASSA request.getRemoteAddr(); {}", request.getRemoteAddr());
-            return ResponseEntity.ok(new SimpleResponseDto("ERROR YOOKASSA"));
-        }
-
         return ResponseEntity.ok(new SimpleResponseDto("SUCCESS"));
     }
 }
--- src/test/java/com/example/testlinux/unit/test/UnitTest.java
+++ src/test/java/com/example/testlinux/unit/test/UnitTest.java
@@ -1,42 +1,15 @@
 package com.example.testlinux.unit.test;
 
 import com.example.testlinux.domain.UserNew;
-import com.example.testlinux.dto.UserDto;
-import com.example.testlinux.repository.UserNewRepository;
 import com.example.testlinux.service.UserService;
 
-import com.example.testlinux.service.YooKassaService;
+
 import com.example.testlinux.service.type.extend.TestExtendsMethodTypes;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
-/*import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;*/
-/*import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Base64;*/
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -47,7 +20,6 @@ import java.util.UUID;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 //@PropertySource("classpath:application.properties") //not working
@@ -57,9 +29,6 @@ public class UnitTest {
 
     @InjectMocks
     private UserService userService;
-
-    @InjectMocks
-    private YooKassaService yooKassaService;
 
     private SolutionTest solutionTest = new SolutionTest();
 
@@ -121,39 +90,6 @@ public class UnitTest {
 
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    @Test
-    void testRestTemplateYouKassa(){
-        yooKassaService.registerPayment();
-        //"id" : "2e89b7b0-000f-5000-a000-10d595a2ce4d"
-    }
-
-    @Test
-    void testGetPaymentDataYouKassa(){
-        //"id" : "2e8a2610-000f-5000-a000-122fc0aa0cea"
-        yooKassaService.getPaymentDetails("2e8a2610-000f-5000-a000-122fc0aa0cea");
-        yooKassaService.getPaymentDetails("2e88d1c7-000f-5000-9000-16526fd4dad5");
-        //        2e88d1c7-000f-5000-9000-16526fd4dad5
-    }
-
-    @Test
-    void testHandleNotification(){
-        List<String> allowedIps = List.of(
-                "77.75.156.11",
-                "77.75.156.35",
-                "185.71.76.5", // Доступные хосты: 185.71.76.1 - 185.71.76.30
-                "185.71.76.2", // Доступные хосты: 185.71.76.1 - 185.71.76.30
-                "185.71.76.25", // Доступные хосты: 185.71.76.1 - 185.71.76.30
-                "185.71.77.2",
-                "77.75.153.21", // Доступные хосты: 77.75.153.1 - 77.75.153.126
-                "77.75.154.129",
-                "77.75.154.222",
-                "2a02:5180::ffff"
-        );
-        for(String allowedIp : allowedIps) {
-            assertTrue(yooKassaService.isIpAllowed(allowedIp));
         }
     }
 
@@ -237,33 +173,4 @@ public class UnitTest {
         int remainsder = i % max;
         log.info("remainder: {}", remainsder);
     }
-    /*@Test
-    public Map<String, String> getValuesOfResponse(FighterPaymentDto clientDto) throws JsonProcessingException {
-        String orderNumber = UUID.randomUUID().toString().replace("-", "");
-        log.info("orderNumber; {}", orderNumber);
-        RestTemplate restTemplate = new RestTemplate();
-        Map<String, Object> testReg = new HashMap<>();
-        testReg.put("userName", SberPayData.LOGIN);
-        testReg.put("password", SberPayData.PASSWORD);
-        testReg.put("orderNumber", orderNumber);
-        testReg.put("returnUrl", "https://strikerstat.com/sberPay/urlSuccess.php?id=" + clientDto.getIdEvent());
-        testReg.put("failUrl", "https://strikerstat.com/sberPay/urlFail.php?id=" + clientDto.getIdEvent());
-        testReg.put("amount", clientDto.getDeposit() * 100);
-
-        String url = "https://securepayments.sberbank.ru/payment/rest/register.do";
-
-        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
-        testReg.forEach((key, value) -> formData.add(key, value.toString()));
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(formData, headers);
-        String response = restTemplate.postForObject(url, request, String.class);
-        log.info("PayService restTemplate.postForObject response; {}", response);
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        return mapper.readValue(response, new TypeReference<Map<String, String>>() {});
-    }*/
 }
