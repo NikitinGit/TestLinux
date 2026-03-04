d9470c8e462432067f8689ad3c4fef482be8ddc6
igor <andnikitn5@gmail.com>
2026-03-04 12:00:59 +0500

    rgb -> int

--- src/main/java/com/example/testlinux/bit/wise/BitWiseOperators.java
+++ src/main/java/com/example/testlinux/bit/wise/BitWiseOperators.java
@@ -17,5 +17,29 @@ public class BitWiseOperators {
         System.out.println("~d; " + (~d));
         System.out.println("Двоичный вид (~d): " + Integer.toBinaryString((~d)));
 
+        System.out.println("~d + 1; " + (~d + 1));
+
+        int f = -5;
+        System.out.println("~f + 1; " + (~f + 1));
+
+        int red = 255;
+        int green = 128;
+        int blue = 64;
+
+        // Сдвигаем и объединяем
+        int rgb1 = (red << 16) | (green << 8) | blue;
+        int rgb2 = (red << 16) + (green << 8) + blue;
+        System.out.println("rgb1; " + rgb1 + "\nrgb2; " + rgb2);
+
+        int mask = 0xFF;
+        System.out.println("Integer.toBinaryString(mask); " + Integer.toBinaryString(mask));
+
+        System.out.println("Integer.toBinaryString(rgb1); " + Integer.toBinaryString(rgb1));
+
+        int green2 = (rgb1 >> 8) & mask;
+        System.out.println("Integer.toBinaryString(green2); " + Integer.toBinaryString(green2));
+
+        int blue2 = rgb1 & mask;
+        System.out.println("blue2; " + blue2);
     }
 }
