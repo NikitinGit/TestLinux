8413066f34b32c8b3b4a11ea5b781c2d489451c7
igor <andnikitn5@gmail.com>
2026-05-13 18:42:27 +0500

    FactoryMethod with state

--- src/main/java/com/example/testlinux/java/core/factory/Logistics.java
+++ src/main/java/com/example/testlinux/java/core/factory/Logistics.java
@@ -6,14 +6,15 @@ public abstract class Logistics {
     private final String region;
 
     protected Logistics(String region) {
+        System.out.println("Logistics(String region): " + region);
         this.region = region;
     }
 
     abstract Transport createTransport(); // фабричный метод — подкласс обязан реализовать
 
     // общая логика: оба подкласса используют этот метод БЕЗ переопределения
-    void deliver(String name) {
-        System.out.println("Region: " + region);           // используем поле
+    public void deliver(String name) {
+        System.out.println("Logistics.deliver(), this.region;  " + this.region);           // используем поле
         System.out.println("before Transport t = createTransport(); ..." + name);
         Transport t = createTransport(); // вызывает фабричный метод
         t.move();
