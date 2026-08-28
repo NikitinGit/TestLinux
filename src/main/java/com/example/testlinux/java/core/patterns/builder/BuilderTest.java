f8895e2a83bd0d56c94957604bf25684a1a8cb53
igor <andnikitn5@gmail.com>
2026-08-28 19:41:31 +0500

    C3 base

--- src/main/java/com/example/testlinux/java/core/patterns/builder/BuilderTest.java
+++ src/main/java/com/example/testlinux/java/core/patterns/builder/BuilderTest.java
@@ -3,7 +3,14 @@ package com.example.testlinux.java.core.patterns.builder;
 public class BuilderTest {
 
     public static void main(String[] args) {
-        System.out.println("commit from feature branch for test rebase #1");
-        System.out.println("commit from master branch for test rebase #1");
+        System.out.println("FEATURE log: v0");
+
+        printSeparator();
+
+        System.out.println("BASE log: v3");
+    }
+
+    private static void printSeparator() {
+        System.out.println("--------------------------------");
     }
 }
