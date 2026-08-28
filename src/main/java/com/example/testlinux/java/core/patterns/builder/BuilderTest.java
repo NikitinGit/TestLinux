617bd6f0c72235612573c28cb499a84174899253
igor <andnikitn5@gmail.com>
2026-08-28 19:16:38 +0500

    Builder Test

---
+++ src/main/java/com/example/testlinux/java/core/patterns/builder/BuilderTest.java
@@ -0,0 +1,9 @@
+package com.example.testlinux.java.core.patterns.builder;
+
+public class BuilderTest {
+
+    public static void main(String[] args) {
+        System.out.println("commit from feature branch for test rebase #1");
+        System.out.println("commit from master branch for test rebase #1");
+    }
+}
