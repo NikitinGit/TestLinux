73fbfe1c2cdca7f0027d5451b227e964f61abf45
igor <andnikitn5@gmail.com>
2026-08-29 12:29:39 +0500

    DEVELOP CREATE --------------------

--- src/main/java/com/example/testlinux/java/core/patterns/builder/BuilderTest.java
+++ src/main/java/com/example/testlinux/java/core/patterns/builder/BuilderTest.java
@@ -7,7 +7,7 @@ public class BuilderTest {
 
         printSeparator();
 
-        System.out.println("BASE log: v3");
+        System.out.println("DEVELOP log: v0");
     }
 
     private static void printSeparator() {
